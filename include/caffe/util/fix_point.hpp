2d10427de2b0d25cbf8e5d9d55661250a4d433e0
ttq <tianqi.tang@deephi.tech>
2017-02-02 21:38:25 +0800

    fix_point

--- include/caffe/util/fix_point.hpp
+++ include/caffe/util/fix_point.hpp
@@ -4,20 +4,21 @@
 namespace caffe {
 
 template <typename Dtype>
-void caffe_cpu_fix(const int n, Dtype*x, Dtype* y, const int bit_level, const int scale);
+void caffe_cpu_fix(const int n, const Dtype*x, Dtype* y, const int bit_level, const int scale);
 
-//#ifndef CPU_ONLY
+#ifndef CPU_ONLY
 
-//template <typename Dtype>
-//void caffe_gpu_fix(const int n, const Dtype*x, Dtype* y, const int bit_level, const int scale);
+template <typename Dtype>
+void caffe_gpu_fix(const int n, const Dtype*x, Dtype* y, const int bit_level, const int scale);
 
-//template <typename Dtype>
-//void caffe_gpu_fix_diff_w(const int n, const Dtype* x, Dtype* y, const int bit_level, const int max_scale, const int min_scale, int &final_scale);
+/*
+template <typename Dtype>
+void caffe_gpu_fix_diff_w(const int n, const Dtype* x, Dtype* y, const int bit_level, const int max_scale, const int min_scale, int &final_scale);
 
-//template <typename Dtype>
-//void caffe_gpu_fix_overflow(const int n, const Dtype* x, Dtype* y, const int bit_level, const int max_scale, const int min_scale, int& final_scale);
-
-//#endif  // !CPU_ONLY
+template <typename Dtype>
+void caffe_gpu_fix_overflow(const int n, const Dtype* x, Dtype* y, const int bit_level, const int max_scale, const int min_scale, int& final_scale);
+*/
+#endif  // !CPU_ONLY
 
 }  // namespace caffe
 
